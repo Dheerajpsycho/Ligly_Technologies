493b863b928ceb2f34e8f2cc589b2c3059a92694
Dheerajpsycho <80106107+Dheerajpsycho@users.noreply.github.com>
2022-05-16 06:58:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("style/style.css")
+local_css("style.css")
 
 # ---- LOAD ASSETS ----
 lottie_coding = load_lottieurl('https://assets7.lottiefiles.com/packages/lf20_w51pcehl.json')
